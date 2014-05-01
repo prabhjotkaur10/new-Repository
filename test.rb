ad57960cf3afebc91c96a775e389a6403949f88b
Prabhjot <prabhjot@addvalsolutions.com>
2014-05-01 16:52:07 +0530

    1st commit

---
+++ test.rb
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js">
+</script>
+<script> 
+	$(document).ready(function(){
+	 	$("button").click(function(){
+	    	$("div").animate({
+	      		height:'+=150px',
+	      		width:'+=150px'
+	    	});
+	   		$("div").css("background-color","red");
+	  	});
+});
+</script> 
+</head>
+ 
+<body>
+	<button>Start Animation</button>
+	<div style="background:#98bf21;height:100px;width:100px;position:absolute;">
+	</div>
+</body>
+</html>
